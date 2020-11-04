48d2b0bb23bdea88d7b8a07a8cf49ec209f2e545
ChristanWill123 <christanwill123@gmail.com>
2020-11-04 11:55:32 -0500

    Add files via upload
    Main files for the project

---
+++ app/app.js
@@ -0,0 +1,54 @@
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("Slides");
+  
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+ 
+}
+
+
+//Function to scroll from header to video section
+function scrolltovideos(){
+  document.querySelector('.videos').scrollIntoView({
+     behavior: 'smooth'
+  });
+
+}
+
+//Function for validating form completion
+
+function validator(){
+  const mail = document.forms["subscriberForm"]['email'].value;
+  const country = document.getElementById("country").value;
+
+  if((mail == '' || mail == 'EMAIL ADDRESS')) {
+    alert("Email address must be filled out");
+    return false;
+  } else if (country == 'COUNTRY'){
+    alert("Please enter in country to continue");
+    return false;
+  } else{
+    alert("Thank You for submitting!");
+    return true;
+  }
+
+}
